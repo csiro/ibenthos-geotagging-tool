2064d486afc90f938a527bdafcac29684c210d4f
Brendan Do <brendan.do@csiro.au>
2025-05-22 00:08:47 +1000

    Added a build script

---
+++ build_x86.zsh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+set -e
+
+python3 -m venv x86_build
+source x86_build/bin/activate
+
+arch -x86_64 pip install pyqt6 pyexiftool pyyaml pyinstaller
+arch -x86_64 pip install git+ssh://git@bitbucket.csiro.au:7999/visage/geotag_pt.git
+
+git rev-parse HEAD > x86_build/build_id.txt
+
+pyinstaller --clean --noconfirm main.spec
+codesign --force --sign "Mac Developer: Brendan Do (V4NYX6N27N)" "./dist/iBenthos Geotagging Tool.app"
+
+cd dist
+
+ditto -c -k --sequesterRsrc --keepParent "iBenthos Geotagging Tool.app" "iBenthos Geotagging Tool x86 $(cat ../x86_build/build_id.txt).zip"
+
+deactivate
+
+cd ..
+
+rm -rf x86_build
