6426956ca456bb5b6e5cc8b60b0b5cd0f9eaccf8
Brendan Do <brendan.do@csiro.au>
2025-06-11 17:07:46 +1000

    Use poetry for building

--- build_x86.ps1
+++ build_x86.ps1
@@ -3,13 +3,12 @@ $EXIFTOOL_VERSION = "13.29"
 $EXIFTOOL_ARCHIVE_NAME = "exiftool-${EXIFTOOL_VERSION}_64"
 $BUILD_DIR_NAME = "x86_build"
 
-# Create and activate virtual environment
+# Create new virtual environment
 python -m venv $BUILD_DIR_NAME
-& "$BUILD_DIR_NAME\Scripts\Activate.ps1"
 
 # Install Python packages
-pip install pyside6 pyexiftool pyyaml pyinstaller
-pip install git+ssh://git@bitbucket.csiro.au:7999/visage/geotag_pt.git
+poetry env use x86_build\Scripts\python.exe
+poetry install
 
 # Save the current git commit hash
 $buildId = git rev-parse HEAD
@@ -21,13 +20,10 @@ Expand-Archive -Path "$BUILD_DIR_NAME\$EXIFTOOL_ARCHIVE_NAME.zip" -DestinationPa
 Rename-Item -Path "$BUILD_DIR_NAME\$EXIFTOOL_ARCHIVE_NAME\exiftool(-k).exe" -NewName "exiftool.exe"
 
 # Run PyInstaller
-pyinstaller --clean --noconfirm main.spec
+poetry run pyinstaller --clean --noconfirm main.spec
 
 # Compile the installer
 ISCC.exe /O"dist" ".\windows_setup_builder.iss"
-
-# Deactivate virtual environment
-deactivate
 
 # clean up
 Remove-Item -Recurse -Force x86_build
