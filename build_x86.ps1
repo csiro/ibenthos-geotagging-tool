f048f9bc3b243553c68480bed86ab845e6ecc848
Brendan Do <brendan.do@csiro.au>
2025-06-13 10:10:24 +1000

    Build script improvements.

--- build_x86.ps1
+++ build_x86.ps1
@@ -3,12 +3,8 @@ $EXIFTOOL_VERSION = "13.29"
 $EXIFTOOL_ARCHIVE_NAME = "exiftool-${EXIFTOOL_VERSION}_64"
 $BUILD_DIR_NAME = "x86_build"
 
-# Create new virtual environment
-python -m venv $BUILD_DIR_NAME
-
-# Install Python packages
-poetry env use x86_build\Scripts\python.exe
-poetry install
+# Make a temp build directory
+New-Item -ItemType Directory -Path $BUILD_DIR_NAME -Force
 
 # Save the current git commit hash
 $buildId = git rev-parse HEAD
@@ -19,11 +15,14 @@ Invoke-WebRequest -Uri "https://exiftool.org/$EXIFTOOL_ARCHIVE_NAME.zip" -OutFil
 Expand-Archive -Path "$BUILD_DIR_NAME\$EXIFTOOL_ARCHIVE_NAME.zip" -DestinationPath $BUILD_DIR_NAME -Force
 Rename-Item -Path "$BUILD_DIR_NAME\$EXIFTOOL_ARCHIVE_NAME\exiftool(-k).exe" -NewName "exiftool.exe"
 
+# Install Python packages
+poetry install
+
 # Run PyInstaller
 poetry run pyinstaller --clean --noconfirm main.spec
 
 # Compile the installer
 ISCC.exe /O"dist" ".\windows_setup_builder.iss"
 
-# clean up
+# Clean up
 Remove-Item -Recurse -Force x86_build
--- build_x86.zsh
+++ build_x86.zsh
@@ -7,27 +7,26 @@ BUILD_DIR_NAME="x86_build"
 
 set -e
 
-python3 -m venv $BUILD_DIR_NAME
-source $BUILD_DIR_NAME/bin/activate
+# Make a temp build directory
+mkdir -p $BUILD_DIR_NAME
 
-arch -x86_64 pip install pyside6 pyexiftool pyyaml pyinstaller
-arch -x86_64 pip install git+ssh://git@bitbucket.csiro.au:7999/visage/geotag_pt.git
-
+# Grab the git hash of the current commit
 git rev-parse HEAD > $BUILD_DIR_NAME/build_id.txt
 
 # Download exiftool
 wget -O "$BUILD_DIR_NAME/$EXIFTOOL_ARCHIVE_NAME" "https://exiftool.org/$EXIFTOOL_ARCHIVE_NAME"
 tar -xzvf $BUILD_DIR_NAME/$EXIFTOOL_ARCHIVE_NAME -C $BUILD_DIR_NAME
 
-pyinstaller --clean --noconfirm main.spec
-# codesign --force --sign "Mac Developer: Brendan Do (V4NYX6N27N)" "./dist/iBenthos Geotagging Tool.app"
+# Install Python packages
+arch -x86_64 poetry install
 
+# Generate the binary
+arch -x86_64 poetry run pyinstaller --clean --noconfirm main.spec
+
+# Create the zip file
 cd dist
-
 ditto -c -k --sequesterRsrc --keepParent "iBenthos Geotagging Tool.app" "iBenthos Geotagging Tool x86 $(cat ../$BUILD_DIR_NAME/build_id.txt).zip"
-
-deactivate
-
 cd ..
 
+# Clean up build files
 rm -rf $BUILD_DIR_NAME
